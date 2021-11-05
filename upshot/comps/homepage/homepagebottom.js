ae18f968e405ba449029ec62a75c482f9f492233
patrick brodie <patrickxbrodie@hotmail.com>
2021-11-05 16:40:49 -0400

    added boiler plate for home page comps and imgs

---
+++ upshot/comps/homepage/homepagebottom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeBottom = () => {
+    
+    return (
+<div>
+    This is the Bottom of the home page
+</div>
+    );
+};
+
+export default HomeBottom;
---
+++ upshot/images/index.js
@@ -0,0 +1,10 @@
+import graph1 from './graph1.png'
+import graph2 from './graph2.png'
+
+
+const imgs = {
+    graph1,
+    graph2
+}
+
+export default imgs
